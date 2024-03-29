dcaa45d80b0ab82af00408dde0ab8abce9efc9c2
TariqMehmood1004 <116074810+TariqMehmood1004@users.noreply.github.com>
2024-03-29 23:21:57 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/lifeopti/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lifeopti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import './utils/apis.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'VikVok',
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark(),
+      theme: ThemeData(),
+      home: const MyHomePage(title: 'VikVok'),
+    ),
+  );
+}
